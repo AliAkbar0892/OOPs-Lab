d43a99a6e30cc0e7245b83211973648eda6cded0
Ali Akbar <aliakbarmak1405@gmail.com>
2025-02-19 13:29:53 +0500

    Update l2p5.cpp

--- Lab-2/l2p5.cpp
+++ Lab-2/l2p5.cpp
@@ -6,35 +6,27 @@
 */
 
 #include <iostream>
-
 using namespace std;
 
 int main() {
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
-
     int *arr = new int[n];
-
     cout << "Enter " << n << " integers:\n";
     for (int i = 0; i < n; i++) {
         cin >> *(arr + i);
     }
-
     cout << "\nArray elements: ";
     for (int i = 0; i < n; i++) {
         cout << *(arr + i) << " ";
     }
     cout << endl;
-
     int sum = 0;
     for (int i = 0; i < n; i++) {
         sum += *(arr + i);
     }
-
     cout << "Sum of array elements: " << sum << endl;
-
     delete[] arr;
-
     return 0;
 }
